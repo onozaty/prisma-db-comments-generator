d1b18008f265b406940819941058df800bfb973f
onozaty <onozaty@gmail.com>
2024-08-02 22:45:35 +0900

    Add comment difference extraction process

---
+++ src/comment.test.ts
@@ -0,0 +1,470 @@
+import { Comments, createComments, diffComments } from "./comment";
+import { Model } from "./parser";
+
+describe("createComments", () => {
+  test("empty", () => {
+    // ARRANGE
+    const models: Model[] = [];
+
+    // ACT
+    const comments = createComments(models);
+
+    // ASSERT
+    expect(comments).toStrictEqual({});
+  });
+
+  test("normal", () => {
+    // ARRANGE
+    const models: Model[] = [
+      {
+        dbName: "table1",
+        documentation: "table1 comment",
+        fields: [
+          { dbName: "field1", documentation: "field1 comment" },
+          { dbName: "field2" },
+          {
+            dbName: "field3",
+            documentation: "field3 comment",
+            typeEnum: {
+              dbName: "enum1",
+              name: "enum1_name",
+              values: ["A", "B"],
+              documentation: "enum1 comment",
+            },
+          },
+          {
+            dbName: "field4",
+            typeEnum: {
+              dbName: "enum2",
+              name: "enum2_name",
+              values: ["A", "B", "C"],
+            },
+          },
+        ],
+      },
+      {
+        dbName: "table2",
+        fields: [{ dbName: "fieldA", documentation: "fieldA comment" }],
+      },
+    ];
+
+    // ACT
+    const comments = createComments(models);
+
+    // ASSERT
+    expect(comments).toStrictEqual({
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    });
+  });
+});
+
+describe("diffComments", () => {
+  test("empty", () => {
+    // ARRANGE
+    const first: Comments = {};
+    const second: Comments = {};
+
+    // ACT
+    const comments = diffComments(first, second);
+
+    // ASSERT
+    expect(comments).toStrictEqual({});
+  });
+
+  test("empty second", () => {
+    // ARRANGE
+    const first: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+    const second: Comments = {};
+
+    // ACT
+    const comments = diffComments(first, second);
+
+    // ASSERT
+    expect(comments).toStrictEqual({
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+        ],
+      },
+      table2: {
+        table: undefined,
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    });
+  });
+
+  test("empty first", () => {
+    // ARRANGE
+    const first: Comments = {};
+    const second: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+
+    // ACT
+    const comments = diffComments(first, second);
+
+    // ASSERT
+    expect(comments).toStrictEqual({});
+  });
+
+  test("same", () => {
+    // ARRANGE
+    const first: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+    const second: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+
+    // ACT
+    const comments = diffComments(first, second);
+
+    // ASSERT
+    expect(comments).toStrictEqual({});
+  });
+
+  test("diff", () => {
+    // ARRANGE
+    const first: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment xxx",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "xxx",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "xxx",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+    const second: Comments = {
+      table1: {
+        table: {
+          tableName: "table1",
+          comment: "table1 comment",
+        },
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "field3 comment",
+          },
+          {
+            tableName: "table1",
+            columnName: "field4",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "",
+        },
+        columns: [
+          {
+            tableName: "table2",
+            columnName: "fieldA",
+            comment: "fieldA comment",
+          },
+        ],
+      },
+    };
+
+    // ACT
+    const comments = diffComments(first, second);
+
+    // ASSERT
+    expect(comments).toStrictEqual({
+      table1: {
+        table: undefined,
+        columns: [
+          {
+            tableName: "table1",
+            columnName: "field1",
+            comment: "field1 comment xxx",
+          },
+          {
+            tableName: "table1",
+            columnName: "field2",
+            comment: "xxx",
+          },
+          {
+            tableName: "table1",
+            columnName: "field3",
+            comment: "",
+          },
+        ],
+      },
+      table2: {
+        table: {
+          tableName: "table2",
+          comment: "xxx",
+        },
+        columns: [],
+      },
+    });
+  });
+});
---
+++ src/comment.ts
@@ -0,0 +1,98 @@
+import { Model } from "./parser";
+
+export type Comments = {
+  [key: string]: TableComments;
+};
+
+export type TableComments = {
+  table?: TableComment;
+  columns?: ColumnComment[];
+};
+
+export type TableComment = {
+  tableName: string;
+  comment: string;
+};
+
+export type ColumnComment = {
+  tableName: string;
+  columnName: string;
+  comment: string;
+};
+
+export const createComments = (models: Model[]): Comments => {
+  const comments: Comments = {};
+
+  for (const model of models) {
+    comments[model.dbName] = {
+      table: {
+        tableName: model.dbName,
+        comment: model.documentation ?? "",
+      },
+      columns: model.fields.map((field) => {
+        return {
+          tableName: model.dbName,
+          columnName: field.dbName,
+          comment: field.documentation ?? "",
+        };
+      }),
+    };
+  }
+
+  return comments;
+};
+
+export const diffComments = (first: Comments, second: Comments): Comments => {
+  // first の中で、second と同じものは除外
+  // first でコメントが空で、かつsecondに対応するものが無かった場合も除外
+  // (空のコメントは、前回コメントが空じゃなかったときだけ入れるので)
+  const diff: Comments = {};
+
+  for (const key in first) {
+    const tableDiff = diffTableComments(first[key], second[key]);
+    if (tableDiff) {
+      diff[key] = tableDiff;
+    }
+  }
+
+  return diff;
+};
+
+const diffTableComments = (
+  first: TableComments,
+  second?: TableComments,
+): TableComments | undefined => {
+  const tableDiff = diffComment(first.table!, second?.table);
+  const commentDiffs = first.columns
+    ?.map((firstColumn) => {
+      // カラムが一致するものを探して比較
+      const secondColumn = second?.columns?.find(
+        (x) => x.columnName === firstColumn.columnName,
+      );
+      return diffComment(firstColumn, secondColumn);
+    })
+    .filter((x) => x !== undefined);
+
+  if (!tableDiff && (!commentDiffs || commentDiffs.length === 0)) {
+    return undefined;
+  }
+
+  return {
+    table: tableDiff,
+    columns: commentDiffs,
+  };
+};
+
+const diffComment = <T extends TableComment | ColumnComment>(
+  first: T,
+  second?: T,
+): T | undefined => {
+  if (different(first.comment, second?.comment)) {
+    return { ...first };
+  }
+  return undefined;
+};
+
+const different = (first: string, second?: string) => {
+  return first !== (second ?? "");
+};
