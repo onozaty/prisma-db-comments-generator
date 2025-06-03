dac6897a7e8e888b8314180214858503e377be38
onozaty <onozaty@gmail.com>
2025-06-03 23:48:00 +0900

    chore: refine coverage targets

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    coverage: { provider: "v8", reporter: ["text", "clover", "json", "lcov"] },
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: ["**/*.test.ts"],
+      reporter: ["text", "clover", "json", "lcov"],
+    },
   },
 });
